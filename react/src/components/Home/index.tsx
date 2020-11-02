d604089359eb03a1e7a9c352e4864a5c73d83cc6
Hiro-mackay <johnmackay150@gmail.com>
2020-11-02 21:05:36 +0900

    コンポーネントのテンプレート作成 ./react/src/cmoponents/Home/index.tsx

---
+++ react/src/components/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+export const Home = () => {
+  return (
+    <div>
+      <H1></H1>
+    </div>
+  );
+};
+
+const H1 = styled.h1`
+  font-size: 16px;
+`;
