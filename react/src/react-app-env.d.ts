06ce2fe45d140cc848867c44fe93689f839f1db4
Hiro-mackay <johnmackay150@gmail.com>
2020-11-03 21:22:53 +0900

    ローカルファイルへのパス作成

--- react/src/react-app-env.d.ts
+++ react/src/react-app-env.d.ts
@@ -1,1 +1,20 @@
 /// <reference types="react-scripts" />
+declare module "*.png" {
+  const src: string | undefined;
+  export default src;
+}
+
+declare module "*.svg" {
+  const src: string | undefined;
+  export default src;
+}
+
+declare module "*.jpg" {
+  const src: string | undefined;
+  export default src;
+}
+
+declare module "*.jpeg" {
+  const src: string | undefined;
+  export default src;
+}
